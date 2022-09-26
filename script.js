c6ead7d1c43e833c453aa23ffa3af36999365f7d
Nikhil Harry <103459068+nikhilhex@users.noreply.github.com>
2022-09-26 14:38:27 -0700

    Fixed issue 5.
    Fixed #5 and put it right before the number (because I felt it would be easier to remember)

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ word = words[Math.floor(Math.random() * words.length)];
 word = word[0].toUpperCase() + word.substring(1);
 
 
-$("#password").text(sc[Math.floor(Math.random() * sc.length)] + word + Math.floor(Math.random() * 99));
+$("#password").text(word + sc[Math.floor(Math.random() * sc.length)] + Math.floor(Math.random() * 99));
 
 
 
